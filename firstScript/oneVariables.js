6c21b2e3748191bbb82b9436e17f991b929cb8f0
Zarreen. <143486545+TasniyaZarreen23@users.noreply.github.com>
2024-02-12 12:20:02 +0000

    variables

---
+++ firstScript/oneVariables.js
@@ -0,0 +1,11 @@
+const accountId = 1001
+let accountEmail = "xyz@gmail.com"
+var accountPassword = "12345"
+accuntCity = "pune"
+//accountId = 2 // is not alllowd because const value is perminent. it never change again and agan.
+accountEmail =" abc@gainl.com"
+accountPassword ="67890"
+accuntCity = "goa"
+let accountState;//when we dont write any value it means it is undefine
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accuntCity,accountState])
